1e16caf97490cde32d5ea4ffef6b1af84bc91475
pokexavi <xavipba@gmail.com>
2024-09-28 00:53:41 +0200

    Implementation to train the model

--- src/modeling/train.py
+++ src/modeling/train.py
@@ -6,11 +6,15 @@ import mlflow
 import random
 import pandas as pd
 import yaml
-from transformers import ViTForImageClassification, ViTFeatureExtractor
+from transformers import ViTForImageClassification, ViTFeatureExtractor,Trainer, TrainingArguments
 import torch
+import torch.nn as nn
+import torch.utils.data as data
+import numpy as np
 
 from src.config import PROCESSED_DATA_DIR
 
+#TODO DEJARLO BONITO CON FUNCIONES
 
 #We set the experiment name if exists, if not, it will be created
 mlflow.set_experiment("Dogs and cats classification")
@@ -18,13 +22,6 @@ mlflow.sklearn.autolog(log_model_signatures=False, log_datasets=False)
 
 #Path of the parameters file
 params_path = Path("params.yaml")
-
-#Path of the prepared data folder
-input_folder_path = PROCESSED_DATA_DIR
-
-#TODO Read training dataset (data prepocessing should save the files in this folder) (DAVID TIENE QUE GUARDARLO EN ESE PATH)
-X_train = pd.read_csv(input_folder_path / "X_train.csv")
-y_train = pd.read_csv(input_folder_path / "y_train.csv")
 
 #Read data preparation parameters
 with open(params_path, "r", encoding="utf8") as params_file:
@@ -34,7 +31,24 @@ with open(params_path, "r", encoding="utf8") as params_file:
     except yaml.YAMLError as exc:
         print(exc)
 
-#MODEL TRAINING AND EVALUATION
+#Path of the prepared data folder
+input_folder_path = PROCESSED_DATA_DIR
+
+#TODO VER COMO SON  LOS DATOS Y ADAPTAR ESTA PARTE
+X_train = pd.read_csv(input_folder_path / "X_train.csv")
+y_train = pd.read_csv(input_folder_path / "y_train.csv")
+
+x_train_tensor = data.TensorDataset(torch.tensor(X_train.values), torch.tensor(y_train.values))
+y_train_tensor = data.TensorDataset(torch.tensor(X_train.values), torch.tensor(y_train.values))
+
+train_loader = data.DataLoader(x_train_tensor, batch_size=params["batch_size"], shuffle=True,  num_workers=4)
+test_loader  = data.DataLoader(y_train_tensor, batch_size=params["batch_size"], shuffle=False, num_workers=4)
+
+#############################
+#Model training and evaluation
+#############################
+
+
 keys = params["hyperparameters"].keys()
 values = params["hyperparameters"].values()
 combinations = list(itertools.product(*values))
@@ -50,5 +64,24 @@ for comb in random_combinations:
                                                               hidden_size=params["hidden_size"],
                                                               num_hidden_layers=params["num_hidden_layers"],
                                                               num_attention_heads=params["num_attention_heads"])
-        #TODO TIPICO CODIGO DE TRAIN
+        if params["optimizer"] == "Adam":
+            optimizer = torch.optim.Adam(model.parameters(), lr=params["lr"])
+        else:
+            optimizer = torch.optim.SGD(model.parameters(), lr=params["lr"])
 
+        device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+        loss_func = nn.CrossEntropyLoss()
+
+        #Train the model
+        for epoch in range(params["num_epochs"]):        
+            for step, (x, y) in enumerate(train_loader):
+                x = torch.tensor(np.stack(feature_extractor(x.numpy())['pixel_values'], axis=0)).float()
+                x, y  = x.to(device), y.to(device)
+                output = model(x)
+                loss = loss_func(output,y)
+                optimizer.zero_grad()  
+                loss.backward()       
+                optimizer.step() 
+            print(f"Epoch {epoch+1}/{params['num_epochs']}, Loss: {loss.item()}") 
+        
+        #TODO Evaluate model
