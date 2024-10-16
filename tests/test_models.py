3901c07d9a031d654a9b032d1f52c7068ac2c199
pokexavi <xavipba@gmail.com>
2024-10-16 21:53:00 +0200

    add all tests

--- tests/test_models.py
+++ tests/test_models.py
@@ -12,16 +12,113 @@ from torchvision import transforms
 from torch.utils import data
 from PIL import Image
 
-from src.config import METRICS_DIR, MODELS_DIR, PROCESSED_TRAIN_IMAGES,PROCESSED_TEST_IMAGES,PROCESSED_VALID_IMAGES,MODELS_DIR
-from src.models.train import load_params_train
-from src.models.train import combine_hyperparameters
-from src.models.train import get_model, get_optimizer
-from src.models.train import preapare_train_dataloaders
-from src.models.train import training
-from src.models.evaluate import preapare_validation_dataloaders
-from src.models.evaluate import load_image
-from src.models.evaluate import evaluate_model
+from src.config import PROCESSED_DATA_DIR,RAW_DATA_DIR,METRICS_DIR, MODELS_DIR, PROCESSED_TRAIN_IMAGES,PROCESSED_TEST_IMAGES,PROCESSED_VALID_IMAGES,MODELS_DIR
+from src.features.prepare import load_data,load_params_prepare,split_data,save_data
+from src.models.train import load_params_train,combine_hyperparameters,get_model,get_optimizer,preapare_train_dataloaders,training
+from src.models.evaluate import preapare_validation_dataloaders,load_image,evaluate_model
 
+
+
+#Prepare fixtures
+
+@pytest.fixture
+def loaded_data():
+    """
+    Fixture to load the data once and reuse it in multiple tests
+    """
+    df = load_data(RAW_DATA_DIR)
+    return df
+
+
+
+
+@pytest.fixture
+def mock_prepare_parameters():
+    """
+    Returns a dictionary with the parameters for the preprocessing
+    """
+    return {
+        "train_size": 0.01,
+        "test_size": 0.025,
+        "valid_size": 0.7,
+        "random_state": 2024
+    }
+
+
+
+
+@pytest.fixture
+def split_data_mock(loaded_data,mock_prepare_parameters):
+    """
+    Fixture to split the data for the tests.
+    """
+    x_train, x_valid, x_test, y_train, y_valid, y_test = split_data(loaded_data, mock_prepare_parameters)
+    return x_train, x_valid, x_test, y_train, y_valid, y_test
+
+
+
+
+@pytest.fixture
+def mock_yaml_file(tmp_path):
+    """
+    Returns the path to a mock yaml file
+    """
+
+    yaml_content = """
+    prepare:
+        train_size: 0.6
+        test_size: 0.5
+        valid_size: 0.5
+        random_state: 2024
+    train:
+        algorithm: VisualTransformer
+        model_name: google/vit-base-patch16-224
+        batch_size: 32
+        num_epochs: 10
+        learning_rate: 0.001
+        optimizer: adam
+        weight_decay: 0.0001
+        momentum: 0.9
+    """
+    yaml_path = tmp_path / "params_test.yaml"
+    yaml_path.write_text(yaml_content)
+    return yaml_path
+
+
+
+
+#Train, validation fixtures
+
+@pytest.fixture
+def sample_image(tmp_path):
+    """
+    Returns the path to a sample image
+    """
+    # Crear una imagen temporal para usar en el test
+    img_path = "data/processed/valid_images/dogs/image_valid_0.jpg"
+    return img_path
+
+
+
+
+@pytest.fixture
+def mock_train_parameters():
+    """
+    Returns a dictionary with the parameters for the training
+    """
+
+    return {
+        "batch_size": 32,
+        "num_epochs": 2,
+        "algorithm": "VisualTransformer",
+        "model_name": "google/vit-base-patch16-224",
+        "learning_rate": 0.001,
+        "optimizer": "adam",
+        "weight_decay": 0.0001,
+        "momentum": 0.9
+    }
+
+#Test fixtures
 
 @pytest.fixture
 def best_model():
@@ -39,6 +136,9 @@ def best_model():
     #return the best model
     with open(MODELS_DIR / best_model_name, "rb") as f:
         return pickle.load(f)
+
+
+
 
 @pytest.fixture
 def cats_dogs_test_data():
@@ -86,57 +186,91 @@ def cats_dogs_test_data():
     return obtain_test_data(PROCESSED_TEST_IMAGES, batch_size)
 
 
+#PREPARE SCRIPT TESTS
 
-
-@pytest.fixture
-def mock_yaml_file(tmp_path):
+def test_load_data(loaded_data):
     """
-    Returns the path to a mock yaml file
+    Function to test the load_data function
     """
-
-    yaml_content = """
-    train:
-        algorithm: VisualTransformer
-        model_name: google/vit-base-patch16-224
-        batch_size: 32
-        num_epochs: 10
-        learning_rate: 0.001
-        optimizer: adam
-        weight_decay: 0.0001
-        momentum: 0.9
-    """
-    yaml_path = tmp_path / "params_test.yaml"
-    yaml_path.write_text(yaml_content)
-    return yaml_path
+    
+    assert not loaded_data.empty, "Dataframe is empty"
+    assert 'image' in loaded_data.columns, "The column 'image' is not present"
+    assert 'labels' in loaded_data.columns, "The column 'labels' is not present"
 
 
-@pytest.fixture
-def mock_train_parameters():
+
+
+def test_load_params_prepare(mock_yaml_file):
     """
-    Returns a dictionary with the parameters for the training
+    Function to test the load_params_prepare function
     """
 
-    return {
-        "batch_size": 32,
-        "num_epochs": 2,
-        "algorithm": "VisualTransformer",
-        "model_name": "google/vit-base-patch16-224",
-        "learning_rate": 0.001,
-        "optimizer": "adam",
-        "weight_decay": 0.0001,
-        "momentum": 0.9
-    }
+    params = load_params_prepare(mock_yaml_file)
+    assert params is not None, "Parameters are not loaded correctly"
+    assert params["valid_size"] == 0.5, "Valid size is not correct"
+    assert params["random_state"] == 2024, "Random state is not correct"
+    assert params["train_size"] == 0.6, "Train size is not correct"
+    assert params["test_size"] == 0.5, "Test size is not correct"
 
 
-@pytest.fixture
-def sample_image(tmp_path):
+
+
+def test_split_data(loaded_data,mock_prepare_parameters):
     """
-    Returns the path to a sample image
+    Function to test the split_data function
     """
-    # Crear una imagen temporal para usar en el test
-    img_path = "data/processed/valid_images/dogs/image_valid_0.jpg"
-    return img_path
 
+    x_train, x_valid, x_test, y_train, y_valid, y_test = split_data(loaded_data, mock_prepare_parameters)
+    assert len(x_train) > 0, "x_train is empty"
+    assert len(x_valid) > 0, "x_valid is empty"
+    assert len(x_test) > 0, "x_test is empty"
+    assert len(y_train) == len(x_train), "Mismatch between x_train and y_train"
+    assert len(y_valid) == len(x_valid), "Mismatch between x_valid and y_valid"
+    assert len(y_test) == len(x_test), "Mismatch between x_test and y_test"
+    total_data_size = len(loaded_data)
+    
+    expected_train_size = int(total_data_size * mock_prepare_parameters['train_size'])
+    expected_valid_test = int(total_data_size * mock_prepare_parameters['test_size'])
+    expected_valid_size = int(expected_valid_test* mock_prepare_parameters['valid_size'])
+    expected_test_size = int(expected_valid_test - expected_valid_size)
+
+    assert len(x_train) == expected_train_size or len(x_train) == expected_train_size+1, f"Train set size is incorrect, expected {expected_train_size}, got {len(x_train)}"
+    assert len(x_valid) == expected_valid_size or len(x_valid) == expected_valid_size+1, f"Validation set size is incorrect, expected {expected_valid_size}, got {len(x_valid)}"
+    assert len(x_test) == expected_test_size or len(x_test) == expected_test_size+1 , f"Test set size is incorrect, expected {expected_test_size}, got {len(x_test)}"
+
+
+
+
+def test_save_data(split_data_mock):
+
+    x_train, x_valid, x_test, y_train, y_valid, y_test = split_data_mock
+    save_data(x_train, y_train, x_valid, y_valid, x_test, y_test)
+    assert (PROCESSED_DATA_DIR / "X_train.csv").exists(), "File X_train.csv does not exist"
+    assert (PROCESSED_DATA_DIR / "y_train.csv").exists(), "File y_train.csv does not exist"
+    assert (PROCESSED_DATA_DIR / "X_valid.csv").exists(), "File X_valid.csv does not exist"
+    assert (PROCESSED_DATA_DIR / "y_valid.csv").exists(), "File y_valid.csv does not exist"
+    assert (PROCESSED_DATA_DIR / "X_test.csv").exists(), "File X_test.csv does not exist"
+    assert (PROCESSED_DATA_DIR / "y_test.csv").exists(), "File y_test.csv does not exist"
+
+    cats_train_dir = PROCESSED_TRAIN_IMAGES / "cats"
+    dogs_train_dir = PROCESSED_TRAIN_IMAGES / "dogs"
+    cats_valid_dir = PROCESSED_VALID_IMAGES / "cats"
+    dogs_valid_dir = PROCESSED_VALID_IMAGES / "dogs"
+    cats_test_dir = PROCESSED_TEST_IMAGES / "cats"
+    dogs_test_dir = PROCESSED_TEST_IMAGES / "dogs"
+
+    assert cats_train_dir.exists(), "Directory cats does not exist in train images"
+    assert dogs_train_dir.exists(), "Directory dogs does not exist in train images"
+    assert cats_valid_dir.exists(), "Directory cats does not exist in valid images"
+    assert dogs_valid_dir.exists(), "Directory dogs does not exist in valid images"
+    assert cats_test_dir.exists(), "Directory cats does not exist in test images"
+    assert dogs_test_dir.exists(), "Directory dogs does not exist in test images"
+    assert len(list(cats_train_dir.glob("*.jpg"))) > 0, "No cat images in train images"
+    assert len(list(dogs_train_dir.glob("*.jpg"))) > 0, "No dog images in train images"
+    assert len(list(cats_valid_dir.glob("*.jpg"))) > 0, "No cat images in valid images"
+    assert len(list(dogs_valid_dir.glob("*.jpg"))) > 0, "No dog images in valid images"
+    assert len(list(cats_test_dir.glob("*.jpg"))) > 0, "No cat images in test images"
+    assert len(list(dogs_test_dir.glob("*.jpg"))) > 0, "No dog images in test images"
 
 
 #TRAINING SCRIPT TESTS
@@ -148,10 +282,14 @@ def test_load_params_train(mock_yaml_file):
 
     params = load_params_train(mock_yaml_file)
     assert params is not None, "Los parámetros no se cargaron correctamente."
-    assert params["algorithm"] == "VisualTransformer", "Algorithm is not correct."
-    assert params["model_name"] == "google/vit-base-patch16-224", "Model name is not correct."
-    assert params["batch_size"] == 32, "Batch size is not correct."
-
+    assert params["algorithm"] == "VisualTransformer", "Algorithm is not correct"
+    assert params["model_name"] == "google/vit-base-patch16-224", "Model name is not correct"
+    assert params["batch_size"] == 32, "Batch size is not correct"
+    assert params["num_epochs"] == 10, "Number of epochs is not correct"
+    assert params["learning_rate"] == 0.001, "Learning rate is not correct"
+    assert params["optimizer"] == "adam", "Optimizer is not correct"
+    assert params["weight_decay"] == 0.0001, "Weight decay is not correct"
+    assert params["momentum"] == 0.9, "Momentum is not correct"
 
 
 
@@ -175,6 +313,7 @@ def test_preapare_train_dataloaders():
     assert batch[0].shape[3] == 224, "The width of the image is not correct"
     assert batch[0].shape[1] == 3, "The number of channels is not correct"
     assert len(train_dataloader) > 0, "Dataloader does not have any data"
+
 
 
 
@@ -208,20 +347,20 @@ def test_get_model():
 
 
 
-def test_get_optimizer():
+
+def test_get_optimizer(best_model):
     """
     Function to test that in get_optimizer funtion, the optimizer is correctly initialized
     """
 
-    model = get_model("VisualTransformer", "google/vit-base-patch16-224", ["cat", "dog"])
     #Example of parameters for th"""e optimizer if the optimizer is adam
-    optimizer = get_optimizer("adam", 0.001, 0.0001, 0.9, model)
+    optimizer = get_optimizer("adam", 0.001, 0.0001, 0.9, best_model)
     assert isinstance(optimizer, torch.optim.Adam), "Optimizer has not the expected type"
     assert optimizer.defaults['lr'] == 0.001, "Learning rate is not correct"
     assert optimizer.defaults['weight_decay'] == 0.0001, "Weight decay is not correct"
     assert 'momentum' not in optimizer.defaults, "Momentum is not correct"
     #Example of parameters for the optimizer if the optimizer is sgd
-    optimizer = get_optimizer("sgd", 0.001, 0.0001, 0.9, model)
+    optimizer = get_optimizer("sgd", 0.001, 0.0001, 0.9, best_model)
     assert isinstance(optimizer, torch.optim.SGD), "Optimizer has not the expected type"
     assert optimizer.defaults['lr'] == 0.001, "Learning rate is not correct"
     assert optimizer.defaults['weight_decay'] == 0.0001, "Weight decay is not correct"
@@ -230,28 +369,21 @@ def test_get_optimizer():
 
 
 
-def test_train(mock_train_parameters):
+def test_train(mock_train_parameters,best_model):
     """
     Function to test the training process
     """
     #Setup
-    model = get_model(mock_train_parameters["algorithm"], mock_train_parameters["model_name"], ["cat", "dog"])
-    optimizer = get_optimizer(
-        mock_train_parameters["optimizer"],
-        mock_train_parameters["learning_rate"],
-        mock_train_parameters["weight_decay"],
-        mock_train_parameters["momentum"],
-        model
-    )
-    device = torch.device("cpu")  # para el test usaremos CPU
+    optimizer = torch.optim.Adam(best_model.parameters(), lr=mock_train_parameters["learning_rate"],
+                                 weight_decay=mock_train_parameters["weight_decay"])
+    device = torch.device("cpu")
     loss_function = torch.nn.CrossEntropyLoss()
 
-    model, loss_per_epoch = training(mock_train_parameters, model, optimizer, device, loss_function)
+    _, loss_per_epoch = training(mock_train_parameters, best_model, optimizer, device, loss_function)
     
     # Verificar que se entrena y que la pérdida por epoch está decreciendo
     assert len(loss_per_epoch) == mock_train_parameters["num_epochs"], "Epochs don't match"
-    assert loss_per_epoch[0] > loss_per_epoch[-1], "La pérdida no está decreciendo después de entrenar."
-
+    assert loss_per_epoch[0] > loss_per_epoch[-1], "The loss is not decreasing"
 
 
 #EVALUATE SCRIPT TESTS
@@ -261,8 +393,9 @@ def test_load_image(sample_image):
     Function to test the load_image function
     """
     img = load_image(sample_image)
-    assert isinstance(img, Image.Image), "La función no retornó una instancia de imagen."
-    assert img.mode == "RGB", "La imagen no está en modo RGB."
+    assert isinstance(img, Image.Image), "The function does not return an image"
+    assert img.mode == "RGB", "The image is not in RGB mode"
+
 
 
 
@@ -288,17 +421,16 @@ def test_preapare_validation_dataloaders():
 
 
 
-def test_evaluate_model():
+
+def test_evaluate_model(best_model):
     """
     It tests the evaluate_model function
     """
-    run_name = "MODEL_1.pkl"
-    with open(MODELS_DIR / run_name, "rb") as pickled_model:
-            model = pickle.load(pickled_model)
     batch_size = 32
-    accuracy = evaluate_model(model, batch_size)
+    accuracy = evaluate_model(best_model, batch_size)
     assert isinstance(accuracy, float), "Accuracy should be a float"
     assert 0 <= accuracy <= 100, "Accuracy should be between a percentage"
+
 
 
 
